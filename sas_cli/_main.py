da29b2f42943d5b093e9f4a4005dbfd9ec8a0b16
Jordan Amos <jordan.amos@justice.qld.gov.au>
2022-11-22 16:27:23 +1000

    Updated _main.py with InvalidFileException; Updated .gitignore

--- sas_cli/_main.py
+++ sas_cli/_main.py
@@ -6,18 +6,22 @@ from saspy import SASsession
 
 
 def run_program(args: argparse.Namespace) -> int:
-
     with open(args.filepath) as f:
         contents_text = f.read()
 
     sas = SASsession()
     sas.submit(contents_text)
     sas.endsas()
+
     return 0
 
 
-def is_valid_file(filepath: str):
+def is_valid_file(filepath: str) -> bool:
     return os.path.exists(filepath) and filepath.endswith(".sas")
+
+
+class InvalidFileException(Exception):
+    pass
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -32,11 +36,13 @@ def main(argv: Sequence[str] | None = None) -> int:
     args = parser.parse_args(argv)
 
     ret = 0
-    if not is_valid_file(args.filepath):
-        raise Exception
-        return 1
 
-    ret |= run_program(args)
+    if is_valid_file(args.filepath):
+        ret |= run_program(args)
+    else:
+        raise InvalidFileException(
+            f"'{args.filepath}' does not exist or is not a valid .sas file"
+        )
 
     return ret
 
