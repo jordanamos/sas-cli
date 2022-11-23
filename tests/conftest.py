e53ead0bcae4ece86870bc3f7b862d62931d92a9
Jordan Amos <jordan.amos@justice.qld.gov.au>
2022-11-24 08:28:16 +1000

    Parametrized test_main_error

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,6 @@ import pytest
 
 
 @pytest.fixture
-def non_existent_file_path():
-    return "./path/to/nonexistent/sas/file.sas"
-
-
-@pytest.fixture
 def temp_sas_file():
-    with tempfile.NamedTemporaryFile(
-        mode="r", suffix=".sas", dir="./testing"
-    ) as temp_file:
+    with tempfile.NamedTemporaryFile(mode="r", suffix=".sas", dir=".") as temp_file:
         yield temp_file
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,14 +5,10 @@ import pytest
 from sas_cli._main import main, valid_sas_file
 
 
-def test_main_error_with_empty_string():
+@pytest.mark.parametrize("input_main", ("", "./path/to/nonexistent/sas/file.sas"))
+def test_main_error(input_main):
     with pytest.raises(SystemExit):
-        main(([""]))
-
-
-def test_main_error_with_non_existent_file(non_existent_file_path):
-    with pytest.raises(SystemExit):
-        main(([non_existent_file_path]))
+        main(([input_main]))
 
 
 def test_valid_sas_file(temp_sas_file):
