fe5e39d5a43254a7e35cf4d7b0a2243f81759a90
Jordan Amos <jordan.amos@justice.qld.gov.au>
2022-11-23 17:13:05 +1000

    Added fixtures; Refactored tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import tempfile
 from argparse import ArgumentTypeError
 
 import pytest
@@ -5,21 +6,33 @@ import pytest
 from sas_cli._main import main, valid_sas_file
 
 
+@pytest.fixture
+def non_existent_file_path():
+    return "./path/to/nonexistent/sas/file.sas"
+
+
+@pytest.fixture
+def temp_sas_file():
+    with tempfile.NamedTemporaryFile(
+        mode="r", suffix=".sas", dir="./testing"
+    ) as temp_file:
+        yield temp_file
+
+
 def test_main_error_with_empty_string():
     with pytest.raises(SystemExit):
         main(([""]))
 
 
-def test_main_error_with_non_existent_file():
+def test_main_error_with_non_existent_file(non_existent_file_path):
     with pytest.raises(SystemExit):
-        main((["./path/to/nonexistent/sas/file.sas"]))
+        main(([non_existent_file_path]))
 
 
-def test_valid_sas_file():
-    test_program_path = "testing/test_program.sas"
-    assert valid_sas_file(test_program_path) == test_program_path
+def test_valid_sas_file(temp_sas_file):
+    assert valid_sas_file(temp_sas_file.name) == temp_sas_file.name
 
 
-def test_valid_sas_file_error():
+def test_valid_sas_file_error(non_existent_file_path):
     with pytest.raises(ArgumentTypeError):
-        valid_sas_file("./path/to/nonexistent/sas/file.sas")
+        valid_sas_file(non_existent_file_path)
