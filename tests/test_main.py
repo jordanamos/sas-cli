0dd72f415907360edf1db2962dab2520eb371a0c
Jordan Amos <jordan.amos@justice.qld.gov.au>
2022-11-23 15:09:38 +1000

    Added testing/ directory; Refactored test_main.py and added valid_sas_file tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,25 @@
+from argparse import ArgumentTypeError
+
 import pytest
 
-from sas_cli._main import main
+from sas_cli._main import main, valid_sas_file
 
 
-def test_main_error_with_empty_string(capsys):
-    with pytest.raises(Exception):
+def test_main_error_with_empty_string():
+    with pytest.raises(SystemExit):
         main(([""]))
+
+
+def test_main_error_with_non_existent_file():
+    with pytest.raises(SystemExit):
+        main((["./path/to/nonexistent/sas/file.sas"]))
+
+
+def test_valid_sas_file():
+    test_program_path = "testing/test_program.sas"
+    assert valid_sas_file(test_program_path) == test_program_path
+
+
+def test_valid_sas_file_error():
+    with pytest.raises(ArgumentTypeError):
+        valid_sas_file("./path/to/nonexistent/sas/file.sas")
