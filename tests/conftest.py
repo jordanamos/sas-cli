ba65e468bf657d1b6b13cafb540e7da373dabdf9
Jordan Amos <jordan.amos@justice.qld.gov.au>
2022-11-24 13:33:59 +1000

    Deleted conftest.py; Refactored test_main.py

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import tempfile
-
-import pytest
-
-
-@pytest.fixture
-def temp_sas_file():
-    with tempfile.NamedTemporaryFile(mode="r", suffix=".sas", dir=".") as temp_file:
-        yield temp_file
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import tempfile
 from argparse import ArgumentTypeError
 
 import pytest
@@ -5,16 +6,28 @@ import pytest
 from sas_cli._main import main, valid_sas_file
 
 
-@pytest.mark.parametrize("input_main", ("", "./path/to/nonexistent/sas/file.sas"))
-def test_main_error(input_main):
+@pytest.fixture
+def temp_sas_file():
+    with tempfile.NamedTemporaryFile(mode="r", suffix=".sas", dir=".") as temp_file:
+        yield temp_file
+
+
+@pytest.fixture
+def temp_file():
+    with tempfile.NamedTemporaryFile(mode="r", dir=".") as temp_file:
+        yield temp_file
+
+
+@pytest.mark.parametrize("test_input", ("", "./path/to/nonexistent/sas/file.sas"))
+def test_main_error(test_input):
     with pytest.raises(SystemExit):
-        main(([input_main]))
+        main(([test_input]))
 
 
 def test_valid_sas_file(temp_sas_file):
     assert valid_sas_file(temp_sas_file.name) == temp_sas_file.name
 
 
-def test_valid_sas_file_error(non_existent_file_path):
+def test_valid_sas_file_error(temp_file):
     with pytest.raises(ArgumentTypeError):
-        valid_sas_file(non_existent_file_path)
+        valid_sas_file(temp_file.name)
