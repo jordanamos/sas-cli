68a0c61ee7dca32f2a4383cafa79c8fd288a0ed6
Jordan Amos <jordan.amos@justice.qld.gov.au>
2022-11-24 17:54:30 +1000

    Added sub command and set and unset working directory options

--- sas_cli/_main.py
+++ sas_cli/_main.py
@@ -1,9 +1,27 @@
 import argparse
+import configparser
 import os
-import pprint
 from typing import Sequence
 
 from saspy import SASsession
+
+CONFIG_FILE = "config.ini"
+
+
+def set_working_directory(directory: str):
+    # TODO write tests
+    with open(CONFIG_FILE, "w") as config_file:
+        config = configparser.ConfigParser()
+        config["working directory"] = {"path": directory}
+        config.write(config_file)
+
+
+def delete_working_dir():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    if config.remove_option(section="working directory", option="path"):
+        with open(CONFIG_FILE, "w") as config_file:
+            config.write(config_file)
 
 
 def valid_sas_file(filepath: str) -> str:
@@ -14,17 +32,27 @@ def valid_sas_file(filepath: str) -> str:
         raise argparse.ArgumentTypeError(message)
 
     if not filepath.endswith(".sas"):
-        raise argparse.ArgumentTypeError(f"'{filepath}' is not a valid .sas file")
-
+        raise argparse.ArgumentTypeError(
+            f"the file '{filepath}' is not a valid .sas file"
+        )
     return filepath
 
 
+def existing_directory(directory):
+    # TODO write tests
+    if not os.path.isdir(directory):
+        raise argparse.ArgumentTypeError(
+            f" the directory '{directory}' is not a valid directory"
+        )
+    return directory
+
+
 def run_program(args: argparse.Namespace) -> int:
-    with open(args.file_path) as f:
-        contents_text = f.read()
+    with open(args.program_path) as f:
+        program_code = f.read()
 
     with SASsession() as sas:
-        result = sas.submit(contents_text)
+        result = sas.submit(program_code)
 
     if args.show_log:
         print(result["LOG"])
@@ -37,16 +65,39 @@ def main(argv: Sequence[str] | None = None) -> int:
         description="Run a SAS program",
     )
     parser.add_argument(
-        "file_path",
+        "-w",
+        "--set-working-dir",
+        metavar="DIR",
+        help="the full path to the directory you wish to work out of",
+        type=existing_directory,
+    )
+    parser.add_argument(
+        "-d", "--unset-working-dir", action="store_true", dest="delete_working_dir"
+    )
+    subparsers = parser.add_subparsers(dest="command")
+
+    run_parser = subparsers.add_parser("run")
+    run_parser.add_argument(
+        "program_path",
         metavar="FILE",
-        help="the full path to the lame SAS (.sas) program you wish to run",
+        help="the full path to the SAS (.sas) program you wish to run",
         type=valid_sas_file,
     )
-    parser.add_argument("--show-log", dest="show_log", action="store_true")
+    run_parser.add_argument("--show-log", dest="show_log", action="store_true")
 
     ret = 0
     args = parser.parse_args(argv)
-    ret = run_program(args)
+    print(args)
+
+    if args.command is None:
+        if args.delete_working_dir:
+            delete_working_dir()
+        if args.set_working_dir:
+            set_working_directory(args.set_working_dir)
+    else:
+        if args.command == "run":
+            ret = run_program(args)
+
     return ret
 
 
