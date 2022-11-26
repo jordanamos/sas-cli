ea31499ba2b4b32b0ccd5cfab3078270bf55c36a
Jordan Amos <jordan.amos@justice.qld.gov.au>
2022-11-26 11:44:40 +1000

    Refactored _main.py config functions

--- sas_cli/_main.py
+++ sas_cli/_main.py
@@ -1,6 +1,8 @@
 import argparse
 import configparser
 import os
+from configparser import NoOptionError, NoSectionError
+from pathlib import Path
 from typing import Sequence
 
 from saspy import SASsession
@@ -8,21 +10,26 @@ from saspy import SASsession
 CONFIG_FILE = "config.ini"
 
 
-def set_working_directory(directory: str):
-    # TODO write tests
+def load_config(args: argparse.Namespace) -> dict:
+
+    Path(CONFIG_FILE).touch()
+
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+
+    if args.delete_working_dir:
+        config.set("", "working_directory", "")
+        print(f"unset current working directory")
+    if args.set_working_dir:
+        config.set("", "working_directory", args.set_working_dir)
+        print(
+            f"current working directory set as '{args.set_working_dir}'. File paths parsed to this program will be relative from this directory"
+        )
+
     with open(CONFIG_FILE, "w") as config_file:
-        config = configparser.ConfigParser()
-        config["working directory"] = {"path": directory}
         config.write(config_file)
 
-
-def delete_working_dir():
-    # TODO write tests
-    config = configparser.ConfigParser()
-    if config.read(CONFIG_FILE):
-        config.remove_option(section="working directory", option="path")
-        with open(CONFIG_FILE, "w") as config_file:
-            config.write(config_file)
+    return dict(config.items("DEFAULT"))
 
 
 def valid_sas_file(filepath: str) -> str:
@@ -48,7 +55,7 @@ def existing_directory(directory: str):
     return directory
 
 
-def run_program(args: argparse.Namespace) -> int:
+def run_program(args: argparse.Namespace, config: dict) -> int:
     with open(args.program_path) as f:
         program_code = f.read()
 
@@ -69,11 +76,23 @@ def main(argv: Sequence[str] | None = None) -> int:
         "-w",
         "--set-working-dir",
         metavar="DIR",
-        help="the full path to the directory you wish to work out of",
+        help="""the full path to the directory you wish to work out of.
+           Convenience option to update config.ini. File paths parsed to this program will
+           be relative from this directory.""",
         type=existing_directory,
     )
     parser.add_argument(
-        "-d", "--unset-working-dir", action="store_true", dest="delete_working_dir"
+        "-d",
+        "--unset-working-dir",
+        action="store_true",
+        dest="delete_working_dir",
+        help="unsets the current working directory",
+    )
+    parser.add_argument(
+        "-g",
+        "--get-working-dir",
+        action="store_true",
+        help="prints the current working directory",
     )
 
     subparsers = parser.add_subparsers(dest="command")
@@ -90,14 +109,14 @@ def main(argv: Sequence[str] | None = None) -> int:
     ret = 0
     args = parser.parse_args(argv)
 
-    if args.command is None:
-        if args.delete_working_dir:
-            delete_working_dir()
-        if args.set_working_dir:
-            set_working_directory(args.set_working_dir)
-    else:
-        if args.command == "run":
-            ret = run_program(args)
+    config = load_config(args)
+
+    if args.get_working_dir:
+        working_dir = config.get("working_directory")
+        print(f"current working directory is '{working_dir}'")
+
+    if args.command == "run":
+        ret = run_program(args, config)
 
     return ret
 
