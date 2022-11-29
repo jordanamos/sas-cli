3f522a01a04e9030a0e98c49efb7133ef29433e6
Jordan Amos <jordan.amos@justice.qld.gov.au>
2022-11-30 09:00:09 +1000

    Removed config_file functionality

--- sas_cli/_main.py
+++ sas_cli/_main.py
@@ -6,30 +6,7 @@ from typing import Sequence
 
 from saspy import SASsession
 
-CONFIG_FILE = "config.ini"
 SAS_CONFIG_PERSONAL = os.path.join(os.getcwd(), "_sas_cfg_personal.py")
-
-
-def load_config(args: argparse.Namespace) -> dict:
-
-    Path(CONFIG_FILE).touch()
-
-    config = configparser.ConfigParser()
-    config.read(CONFIG_FILE)
-
-    if args.delete_working_dir:
-        config.set("", "working_directory", "")
-        print("Current working directory was unset")
-    if args.set_working_dir:
-        config.set("", "working_directory", args.set_working_dir)
-        print(
-            f"Current working directory set as '{args.set_working_dir}'. File paths parsed to this program will be relative from this directory"
-        )
-
-    with open(CONFIG_FILE, "w") as config_file:
-        config.write(config_file)
-
-    return dict(config.items("DEFAULT"))
 
 
 def valid_sas_file(filepath: str) -> str:
@@ -81,7 +58,6 @@ def main(argv: Sequence[str] | None = None) -> int:
     ret = 0
     args = parser.parse_args(argv)
 
-    # config = load_config(args)
     if args.command == "run":
         ret = run_program(args)
 
