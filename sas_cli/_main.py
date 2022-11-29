9ec0982e33d07aeac88e4f2292bf5fa70a9117af
Jordan Amos <jordan.amos@justice.qld.gov.au>
2022-11-29 12:19:43 +1000

    Removed working directory logic

--- sas_cli/_main.py
+++ sas_cli/_main.py
@@ -34,7 +34,7 @@ def load_config(args: argparse.Namespace) -> dict:
 def valid_sas_file(filepath: str) -> bool:
     try:
         open(filepath)
-    except OSError as e:
+    except (FileNotFoundError, OSError) as e:
         message = f"Can't open '{filepath}': {e}"
         raise argparse.ArgumentTypeError(message)
 
@@ -45,58 +45,25 @@ def valid_sas_file(filepath: str) -> bool:
     return True
 
 
-def existing_directory(directory: str):
-    # TODO write tests
-    if not os.path.isdir(directory):
-        raise argparse.ArgumentTypeError(
-            f"The directory '{directory}' is not a valid directory"
-        )
-    return directory
+def run_program(args: argparse.Namespace) -> int:
 
+    with open(args.program_path) as f:
+        program_code = f.read()
 
-def run_program(args: argparse.Namespace, config: dict) -> int:
+    with SASsession() as sas:
+        print(f"\nRunning program: {args.program_path}\n")
+        result = sas.submit(program_code)
 
-    program_path = os.path.join(config.get("working_directory", ""), args.program_path)
-    if valid_sas_file(program_path):
-        print(f"\nRunning program: {program_path}\n")
-        with open(program_path) as f:
-            program_code = f.read()
+    # print(result)
+    if args.show_log:
+        print(result["LOG"])
 
-        with SASsession() as sas:
-            result = sas.submit(program_code)
-
-        # print(result)
-        if args.show_log:
-            print(result["LOG"])
-
-        return 0
+    return 0
 
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser(
         description="Run a SAS program",
-    )
-    parser.add_argument(
-        "-s",
-        "--set-working-dir",
-        metavar="DIR",
-        help="""the full path to the directory you wish to work out of.
-           Convenience option to update config.ini. File paths parsed to this program will
-           be relative from this directory.""",
-        type=existing_directory,
-    )
-    parser.add_argument(
-        "-u",
-        "--unset-working-dir",
-        action="store_true",
-        dest="delete_working_dir",
-        help="unsets the current working directory",
-    )
-    parser.add_argument(
-        "-w",
-        "--get-working-dir",
-        action="store_true",
-        help="prints the current working directory",
     )
 
     subparsers = parser.add_subparsers(dest="command")
@@ -105,24 +72,18 @@ def main(argv: Sequence[str] | None = None) -> int:
     run_parser.add_argument(
         "program_path",
         metavar="FILE",
-        help="the path to the SAS (.sas) program you wish to run relative to the working directory",
+        help="the path to the SAS (.sas) program you wish to run",
+        type=valid_sas_file,
     )
     run_parser.add_argument("--show-log", dest="show_log", action="store_true")
 
     ret = 0
     args = parser.parse_args(argv)
 
-    config = load_config(args)
-    working_dir = config.get("working_directory")
-
-    if args.get_working_dir:
-        if working_dir is None or working_dir == "":
-            print("No current working directory set")
-        else:
-            print(f"Current working directory is '{working_dir}'")
+    # config = load_config(args)
 
     if args.command == "run":
-        ret = run_program(args, config)
+        ret = run_program(args)
 
     return ret
 
